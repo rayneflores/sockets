c18a71dea1e7da52f8ad10b75379db29026fae86
Rayne Flores <raynitoflores@gmail.com>
2024-04-20 17:55:36 -0400

    update cors and main interface

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require("./sockets");
+const cors = require('cors');
 
 class Server {
     constructor() {
@@ -18,7 +19,11 @@ class Server {
     }
 
     middlewares() {
+        // Deploy public path
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        // Enable CORS
+        this.app.use(cors());
     }
 
     configureSocket() {
